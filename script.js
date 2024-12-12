6e7df0bc50a52d5059a6d8ec7b28a6f5cb083c31
macolpo <134499867+macolpo@users.noreply.github.com>
2024-12-12 18:14:05 +0800

    now

---
+++ script.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+    AOS.init();
+
+     // carousel
+     const carouselItems = document.getElementById("carousel-items");
+     const carouselIndicators = document.getElementById("carousel-indicators");
+
+     for (let i = 0; i < 18; i++) {
+       
+       const carouselItem = document.createElement("div");
+       carouselItem.classList.add("carousel-item"); 
+
+       if (i === 0) {
+         carouselItem.classList.add("active");
+       }
+
+       const img = document.createElement("img");
+       img.src = `img/capstone/${i}.png`; 
+       img.classList.add("d-block", "w-100"); 
+       img.alt = `Slide ${i + 1}`;  
+
+       carouselItem.appendChild(img);
+
+       carouselItems.appendChild(carouselItem);
+
+       const button = document.createElement("button");
+       button.type = "button";
+       button.setAttribute("data-bs-target", "#carouselExampleIndicators");
+       button.setAttribute("data-bs-slide-to", i); 
+       button.setAttribute("aria-label", `Slide ${i + 1}`);  
+
+       if (i === 0) {
+         button.classList.add("active");
+         button.setAttribute("aria-current", "true");  
+       }
+
+       carouselIndicators.appendChild(button);
+     }
+
+  
+});
