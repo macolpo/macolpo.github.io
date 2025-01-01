be6a46397569ff440b3665bbdbf43b2f66788302
macolpo <134499867+macolpo@users.noreply.github.com>
2025-01-01 23:52:32 +0800

    update now

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ $(document).ready(function() {
             navbar.removeClass('bg-transparent').addClass('bg-white shadow');
         } else {
             navbar.removeClass('bg-white shadow').addClass('bg-transparent');
+            $('#home').addClass('active')
         }
     });
 
