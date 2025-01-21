92e3882b7c8cdfaea0e02475cf0c3d7704523cc2
macolpo <134499867+macolpo@users.noreply.github.com>
2025-01-22 07:13:26 +0800

    update d

--- script.js
+++ script.js
@@ -65,45 +65,6 @@ $(document).ready(function() {
       carouselIndicators.appendChild(button);
     }
 
-    //dj 
-
-    // const carouselItemsdj = document.getElementById("carousel-itemsdj");
-    // const carouselIndicatorsdj = document.getElementById("carousel-indicatorsdj");
-
-    // for (let i = 0; i < 21; i++) {
-    //   const carouselItem = document.createElement("div");
-    //   carouselItem.classList.add("carousel-item");
-
-    //   if (i === 0) {
-    //     carouselItem.classList.add("active");
-    //   }
-
-    //   const img = document.createElement("img");
-    //   img.src = `img/dj/${i + 1}.png`; 
-    //   img.classList.add("d-block", "col-lg-9","col-md-9", "mx-auto"); 
-    //   img.alt = `Slide ${i + 1}`; 
-
-    //   carouselItem.appendChild(img);
-
-    //   carouselItemsdj.appendChild(carouselItem);
-
-    //   // Create carousel indicator button
-    //   const button = document.createElement("button");
-    //   button.type = "button";
-    //   button.setAttribute("data-bs-target", "#carouselExampleIndicatorsdj");
-    //   button.setAttribute("data-bs-slide-to", i);
-    //   button.setAttribute("aria-label", `Slide ${i + 1}`);
-
-    //   if (i === 0) {
-    //     button.classList.add("active");
-    //     button.setAttribute("aria-current", "true");
-    //   }
-
-    //   carouselIndicatorsdj.appendChild(button);
-    // }
-
-
-     
 
   
 });
